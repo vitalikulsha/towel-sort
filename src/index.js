1fc39631f7fab325cb2a01127b2397c6fe1455c9
vitalikulsha <kulsha_85@mail.ru>
2022-06-24 11:58:58 +0300

    fix: fix bag

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
   const result = [];
+  if(matrix === undefined){
+    return [];
+  }
   for(let i = 0; i < matrix.length; i++){
     i % 2 === 0 ? result.push(...matrix[i]) : result.push(...matrix[i].reverse());
   }
